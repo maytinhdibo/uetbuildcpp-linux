20869e1aea36ed2d281151368e3e5bd2b2b60a54
Natsu Kagami <natsukagami@gmail.com>
2017-10-08 17:37:07 +0700

    Removed implicit sudo calls to prevent accidental installations

--- installuetCpptool.sh
+++ installuetCpptool.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
+
+if [ $EUID -ne 0 ]
+then
+echo -e "Kịch bản này chỉ được chạy với người dùng có quyền hạn. Bạn hãy dùng sudo !"
+exit 1
+fi
+
 echo "Quá trình cài đặt bắt đầu"
-sudo apt-get update
+apt-get update
 echo 'Cài đặt/nâng cấp g++'
-sudo apt-get install build-essential
-sudo apt-get install g++
+apt-get install build-essential
+apt-get install g++
 echo 'Bắt đầu cài đặt UET G++ Build Tool For Student'
-MY_PATH="`dirname \"$0\"`"
-tar -zxvf """${MY_PATH}"/uetcpp.vap""""
-sudo mv uetbuild-c++ /bin/
-sudo chmod +x /bin/uetbuild-c++
+
+mv uetbuild-c++ /bin/
+chmod +x /bin/uetbuild-c++
 echo "Hoàn tất cài đặt"
